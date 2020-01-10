c574ab0dbae484617c0f1f2c0a7f25328043211e
Hernan Chain <hernan.chain01@gmail.com>
2020-01-10 00:31:27 -0500

    Validation of Blank Fields in the principal Form

--- src/components/ToDoForm.js
+++ src/components/ToDoForm.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React, {useState} from 'react';
 
 export default function ToDoForm(props) {
 
@@ -8,70 +8,76 @@ export default function ToDoForm(props) {
     const [priority, setPriority] = useState('low')
 
     const handleSubmit = (e) => {
-        e.preventDefault();
-        let todo = {
-            title: title,
-            responsible: responsible,
-            description: description,
-            priority:priority
+        if (title === '' || responsible === '' || description === '') {
+            e.preventDefault()
+            alert("Empty Fields of Form ")
+        } else {
+            e.preventDefault()
+            let todo = {
+                title: title,
+                responsible: responsible,
+                description: description,
+                priority: priority
+            }
+            props.onAddTodo(todo)
+            setTitle('')
+            setResponsible('')
+            setDescription('')
+            setPriority('low')
         }
-        props.onAddTodo(todo)
-        setTitle('');
-        setResponsible('');
-        setDescription('');
-        setPriority('low')
+
     }
 
 
-return(
-    <div className="card">
-        <form className="card-body" onSubmit={handleSubmit}>
-            <div className="form-group">
-                <input
-                    type="text"
-                    name="Title"
-                    className="form-control"
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                    placeholder="Title"
-                />
-            </div>
-            <div className="form-group">
-                <input
-                    type="text"
-                    name="Responsible"
-                    className="form-control"
-                    value={responsible}
-                    onChange={(e) => setResponsible(e.target.value)}
-                    placeholder="Responsible"
-                />
-            </div>
-            <div className="form-group">
-                <input
-                    type="text"
-                    name="Description"
-                    className="form-control"
-                    value={description}
-                    onChange={(e) => setDescription(e.target.value)}
-                    placeholder="Description"
-                />
-            </div>
-            <div className="form-group">
-                <select
-                    name="Priority"
-                    className="form-control"
-                    value={priority}
-                    onChange={(e) => setPriority(e.target.value)}
+    return (
+        <div className="card">
+            <form className="card-body" onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <input
+                        type="text"
+                        name="Title"
+                        className="form-control"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        placeholder="Title"
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="text"
+                        name="Responsible"
+                        className="form-control"
+                        value={responsible}
+                        onChange={(e) => setResponsible(e.target.value)}
+                        placeholder="Responsible"
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="text"
+                        name="Description"
+                        className="form-control"
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                        placeholder="Description"
+                    />
+                </div>
+                <div className="form-group">
+                    <select
+                        name="Priority"
+                        className="form-control"
+                        value={priority}
+                        onChange={(e) => setPriority(e.target.value)}
                     >
-                    <option>low</option>
-                    <option>medium</option>
-                    <option>high</option>
-                </select>
-            </div>
-            <button type={"submit"} className="btn btn-success">
-                Save
-            </button>
-        </form>
-    </div>
-)
+                        <option>low</option>
+                        <option>medium</option>
+                        <option>high</option>
+                    </select>
+                </div>
+                <button type={"submit"} className="btn btn-success">
+                    Save
+                </button>
+            </form>
+        </div>
+    )
 }
